f540b5e4e5bedc66d4d2f431d7dd1bd3525fbfe7
Donatas Tubutis <dotubutis@gmail.com>
2022-01-10 19:42:57 +0000

    added head.js

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F604)} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x1F621)} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined
+  }
+  return arr[0]
+};
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

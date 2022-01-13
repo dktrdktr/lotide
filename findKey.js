ad32a37542a146ea669319f727c1001c2cdf957b
Donatas Tubutis <dotubutis@gmail.com>
2022-01-13 19:05:44 +0000

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,35 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(
+      `${String.fromCodePoint(
+        0x1f604
+      )} Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      `${String.fromCodePoint(
+        0x1f621
+      )} Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const findKey = (obj, callback) => {
+  return Object.keys(obj).find((item) => callback(obj[item]));
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);

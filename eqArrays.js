4be86ca1ba44afea1de55334a9f886e5ee26918a
Donatas Tubutis <dotubutis@gmail.com>
2022-01-15 14:01:38 +0000

    recursive eqArrays and eqObjects

--- eqArrays.js
+++ eqArrays.js
@@ -4,10 +4,17 @@ const eqArrays = function(arr1, arr2) {
     !Array.isArray(arr1) ||
     !Array.isArray(arr2) ||
     arr1.length !== arr2.length
-  )
+  ) {
     return false;
+  }
   for (let n in arr1) {
-    if (arr1[n] !== arr2[n]) return false;
+    if (Array.isArray(arr1[n])) {
+      if (!eqArrays(arr1[n], arr2[n])) {
+        return false;
+      }
+    } else if (arr1[n] !== arr2[n]) {
+      return false;
+    }
   }
   return true;
 };
@@ -30,3 +37,26 @@ const assertEqual = function(actual, expected) {
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays(
+  [[2, 3], [4]],
+  [[2, 3], [4]]
+), true);
+assertEqual(eqArrays(
+  [[2, 3], [4]],
+  [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays(
+  [[2, 3], [4]],
+  [[2, 3], 4]), false);
+assertEqual(eqArrays(
+  [[1, 2, [[3], 4]], 5, []],
+  [[1, 2, [[3], 4]], 5, []]), true);
+assertEqual(eqArrays(
+  [[1, 2, [[], 4]], 5, []],
+  [[1, 2, [[3], 4]], 5, []]), false);
+assertEqual(eqArrays(
+  [[[[[[[[[[[[[1]]]]]]]]]]]]],
+  [[[[[[[[[[[[[1]]]]]]]]]]]]]), true);
+assertEqual(eqArrays(
+  [[[[[[[[[[[[[1]]]]]]]]]]]]],
+  [[[[[[[[[[[[[2]]]]]]]]]]]]]), false);
--- eqObjects.js
+++ eqObjects.js
@@ -14,23 +14,29 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Shallow array equality check, not checking for nested arrays or arrays of objects that are identical.
 const eqArrays = function(arr1, arr2) {
   if (
     !Array.isArray(arr1) ||
     !Array.isArray(arr2) ||
     arr1.length !== arr2.length
-  )
+  ) {
     return false;
+  }
   for (let n in arr1) {
-    if (arr1[n] !== arr2[n]) return false;
+    if (Array.isArray(arr1[n])) {
+      if (!eqArrays(arr1[n], arr2[n])) {
+        return false;
+      }
+    } else if (arr1[n] !== arr2[n]) {
+      return false;
+    }
   }
   return true;
 };
 
 // Returns true if both objects have identical keys with identical values.
-// Does not implement equality check for objects as values
-// Does not exclude null, arrays or functions
+// Implements equality check for objects as values
+// Not for functions as object values
 const eqObjects = function(obj1, obj2) {
   if (
     typeof obj1 !== "object" ||
@@ -40,13 +46,11 @@ const eqObjects = function(obj1, obj2) {
     return false;
   }
   for (let key of Object.keys(obj1)) {
-    switch (Array.isArray(obj1[key])) {
-    case true:
-      console.log("case true");
+    if (Array.isArray(obj1[key])) {
       if (!eqArrays(obj1[key], obj2[key])) return false;
-      break;
-    case false:
-      console.log("case false");
+    } else if (typeof obj1[key] === "object") {
+      if (!eqObjects(obj1[key], obj2[key])) return false;
+    } else {
       if (obj1[key] !== obj2[key]) return false;
     }
   }
@@ -57,12 +61,39 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
 
-// assertEqual(eqObjects(ab, ba), true);
-// assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd, dc), true);
-// assertEqual(eqObjects(cd, cd2), false);
+assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+// => true
+assertEqual(
+  eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),
+  false
+);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: {k: 1}, z: 1 }, b: 2 }, { a: { y: {k: 1}, z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: {k: 2}, z: 1 }, b: 2 }, { a: { y: {k: 1}, z: 1 }, b: 2 }), false);
+assertEqual(eqObjects(
+  { a: { y: {}, z: 1 }, b: 2 },
+  { a: { y: {}, z: 1 }, b: 2 }),
+true);
+assertEqual(eqObjects(
+  { y: {}, z: 1 },
+  { y: {}, z: {} }
+),
+false);
+
+assertEqual(eqObjects(
+  {a: [[2, 3], [4, 5]]},
+  {a: [[2, 3], [4, 5]]}), true);
+
+assertEqual(eqObjects(
+  {a: [[2, 3], [4, 5]]},
+  {a: [[2, 3], [4]]}), false);

9557cd520fbda273c2dd99e3191cab22faf29c4a
Donatas Tubutis <dotubutis@gmail.com>
2022-01-17 13:52:51 +0000

    modularizing the code

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,17 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`${String.fromCodePoint(0x1F604)} Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `${String.fromCodePoint(
+        0x1f604
+      )} Assertion Passed: ${actual} === ${expected}`
+    );
   } else {
-    console.log(`${String.fromCodePoint(0x1F621)} Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `${String.fromCodePoint(
+        0x1f621
+      )} Assertion Failed: ${actual} !== ${expected}`
+    );
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("cheese", "cheese");
-assertEqual("cheese", "cheeses");
-assertEqual(5, 6);
+module.exports = assertEqual;
